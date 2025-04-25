4453855c30741daf1ec1cc441bab9dac82d60986
jo5huar3 <jrlengineer@outlook.com>
2025-04-25 18:42:13 -0500

    Chapter 2 complete

--- mainLG.py
+++ mainLG.py
@@ -3,19 +3,19 @@ from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langchain_openai import ChatOpenAI
+from BasicToolNode import BasicToolNode
+from langchain_core.tools import tool
 import io
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from langchain_core.tools import StructuredTool
+from pydantic import BaseModel, Field
 
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
     # in the annotation defines how this state key should be updated
     # (in this case, it appends messages to the list, rather than overwriting them)
     messages: Annotated[list, add_messages]
-
-graph_builder = StateGraph(State)
-
-llm = ChatOpenAI(model="gpt-4o")
 
 def chatbot(state: State):
     return {"messages": [llm.invoke(state["messages"])]}
@@ -32,12 +32,70 @@ def RenderLangGraph(graph):
         # This requires some extra dependencies and is optional
         print("exceptoin occured")
 
+class CalculatorInput(BaseModel):
+    a: int = Field(description="first number")
+    b: int = Field(description="second number")
+
+
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers."""
+    print("multiply is called")
+    return a * b
+
+
+calculator = StructuredTool.from_function(
+    func=multiply,
+    name="Calculator",
+    description="multiply numbers",
+    args_schema=CalculatorInput,
+    return_direct=True,
+)
+
+def route_tools(
+    state: State,
+):
+    """
+    Use in the conditional_edge to route to the ToolNode if the last message
+    has tool calls. Otherwise, route to the end.
+    """
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return END
+
+graph_builder = StateGraph(State)
+
+llm = ChatOpenAI(model="o1")
+
+tools = [calculator]
+llm_with_tools = llm.bind_tools(tools)
+tool_node = BasicToolNode(tools=tools)
 # The first argument is the unique node name
 # The second argument is the function or object that will be called whenever
 # the node is used.
 graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", tool_node)
+
+# The `tools_condition` function returns "tools" if the chatbot asks to use a tool, and "END" if
+# it is fine directly responding. This conditional routing defines the main agent loop.
+graph_builder.add_conditional_edges(
+    "chatbot",
+    route_tools,
+    # The following dictionary lets you tell the graph to interpret the condition's outputs as a specific node
+    # It defaults to the identity function, but if you
+    # want to use a node named something else apart from "tools",
+    # You can update the value of the dictionary to something else
+    # e.g., "tools": "my_tools"
+    {"tools": "tools", END: END},
+)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge(START, "chatbot")
-graph_builder.add_edge("chatbot", END)
 graph = graph_builder.compile()
 # Display our LangGraph
 #RenderLangGraph(graph)
